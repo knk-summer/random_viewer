e5af53d8c9c261e47015d468f6782f227a669157
knk-summer <163017021+knk-summer@users.noreply.github.com>
2025-02-24 15:20:23 +0900

    必要なライブラリのインポートとテスト用画像の格納

--- app/RandomSlideshow.py
+++ app/RandomSlideshow.py
@@ -1,6 +1,9 @@
 # ライブラリのインポート（asで別名をつけてる）
 import tkinter as tk
+from tkinter import filedialog
 import customtkinter
+from PIL import Image, ImageFilter
+import os
 
 # tkオブジェクトの作成
 root = tk.Tk()
@@ -19,32 +22,6 @@ root.resizable(True, True)
 
 # ウィンドウの属性（背景色やウィンドウサイズ、タイトルが設定できる）
 root.configure(bg='#333')
-
-# ウィジェットの配置（pack()：ウィジェットを順番に配置）
-# btn1 = tk.Button(root, text='テストボタン1')
-# btn1.pack(side=tk.RIGHT, fill=tk.Y)
-
-# btn2 = tk.Button(root, text='テストボタン2')
-# btn2.pack(side=tk.TOP, fill=tk.X)
-
-# btn3 = tk.Button(root, text='テストボタン3')
-# btn3.pack(side=tk.LEFT, fill=tk.X, expand=True)
-
-# ウィジェットの配置（grid()：ウィジェットを行列形式で配置）
-# label1 = tk.Label(root, text='ラベル1')
-# label1.grid(row=0, column=0)
-
-# btn1 = tk.Button(root, text='テストボタン1')
-# btn1.grid(row=1, column=0)
-
-# btn2 = tk.Button(root, text='テストボタン2')
-# btn2.grid(row=1, column=1)
-
-# btn3 = tk.Button(root, text='テストボタン3')
-# btn3.grid(row=2, column=0, columnspan=2)
-
-# btn4 = tk.Button(root, text='テストボタン4')
-# btn4.grid(row=3, column=0, columnspan=3, sticky=tk.W+tk.E)
 
 # ウィジェットの配置（place()：ウィジェットを座標で配置）
 label1 = tk.Label(root, text='ラベル1')
