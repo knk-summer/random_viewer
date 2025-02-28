b72e7309510e0293350ed221f8b8235136096c8c
knk-summer <163017021+knk-summer@users.noreply.github.com>
2025-02-28 22:58:27 +0900

    ランダムで取得した画像をcanvas上に表示させる処理追加（classは１つにまとめた）

--- app/RandomSlideshow.py
+++ app/RandomSlideshow.py
@@ -9,22 +9,26 @@ import os
 # 【作業】以下をRandomViewerAppクラスとしてまとめる（裏側の処理はここにまとめたい）
 # 【メモ】全画像パス、ランダムで選出した画像パスの２つクラス変数が必要？初期値は空文字で
 class RandomViewerApp:
-    def __init__(self):
+    def __init__(self,root):
+        folder_name = ''
         self.all_images_paths = ''
         self.random_image = ''
+        self.wedget(root)
 
     # フォルダの選択
     def filedialog_clicked(self):
         iDir = os.path.abspath(os.path.dirname(__file__))
         # 選択したフォルダの絶対パスを取得
-        self.folder_name = tk.filedialog.askdirectory(initialdir=iDir)
-        self.load_images(self.folder_name)
+        global folder_name
+        folder_name = tk.filedialog.askdirectory(initialdir=iDir)
+        self.load_images()
         self.random_select_images(self.all_images_paths)
-        print(self.folder_name)       
-        print(self.random_image)
+        # print(self.folder_name)       
+        # print(self.random_image)
+        # img = Image.open(self.random_image)
 
     # 選択したフォルダ内の全画像ファイルの絶対パスを取得
-    def load_images(self, folder_name):
+    def load_images(self):
         # 取得したパスを格納するために空のリストを作成
         paths = []
         # 画像ファイルかの判断に使う
@@ -38,6 +42,9 @@ class RandomViewerApp:
                 if image_path.endswith(exts):
                     paths.append(image_path)
         self.all_images_paths = paths
+        self.random_select_images(self.all_images_paths)
+        # self.display_image()
+
         
         # ※確認用なので後で消す            
         # print(paths)
@@ -48,12 +55,32 @@ class RandomViewerApp:
         # 【作業】クラス変数が格納されているかで分岐（フォルダ選択前にNEXTを押された時用）
     def random_select_images(self, paths):
         self.random_image = random.choice(paths)
+        self.random_image = self.random_image.replace('\\','/')
+        self.random_image = self.random_image.replace('/','//')
+        self.display_image()
 
-class Window:
-    def __init__(self,root):
-    # ウィンドウのタイトル
-        app = RandomViewerApp()
-        self.app = app
+    def display_image(self):
+        img = Image.open(self.random_image)
+
+        # 画像のサイズを取得
+        w = img.width
+        h = img.height
+
+        # 画像の縦幅がcanvasの縦幅より大きい場合リサイズ
+        if w > self.canvas_w:
+            img = img.resize((int(w * (self.canvas_w / w)), int(h * (self.canvas_w / w))))
+            
+        # 画像の横幅がcanvasの横幅より大きい場合リサイズ
+        if h > self.canvas_h:
+            img = img.resize((int(w * (self.canvas_h / h)), int(h * (self.canvas_h / h))))
+
+        # 画像を表示
+
+        self.display_img = ImageTk.PhotoImage(img)
+        self.canvas.create_image(0, 0, image=self.display_img, anchor=tk.NW)
+
+    def wedget(self,root):
+        # ウィンドウのタイトル
         root.title('random_slideshow')
 
         # ウィンドウのサイズ変更の可否
@@ -67,44 +94,63 @@ class Window:
         label1.place(x=100, y=50)
 
         # tk.SE：座標の基準位置をボタン右下に
-        next_btn = tk.Button(root, text='NEXT', font=('', 20))
+        next_btn = tk.Button(root, text='NEXT', font=('', 20), command=self.load_images)
         next_btn.place(relx=0.9, rely=0.9, anchor=tk.SE, width=200, height=40)
         # tk.NE：座標の基準位置をボタン右上に
-        folder_btn = tk.Button(root, text='フォルダ選択', command=app.filedialog_clicked)
+        folder_btn = tk.Button(root, text='フォルダ選択', command=self.filedialog_clicked)
         folder_btn.place(relx=0.9, rely=0.1, anchor=tk.NE, width=200, height=40)
 
         # canvas（画像表示範囲）の作成
-        canvas_w = 600
-        canvas_h = 600
-        canvas = tk.Canvas(root, width=canvas_w, height=canvas_h)
-        canvas.place(x=0, y=0)
+        self.canvas_w = 600
+        self.canvas_h = 600
+        self.canvas = tk.Canvas(root, width=self.canvas_w, height=self.canvas_h,bg='blue')
+        self.canvas.place(x=0, y=0)
 
-        img = Image.open('hoge.jpg')
-        img = ImageTk.PhotoImage(img)
-        canvas.create_image(0, 0, image=img, anchor=tk.NW)
+        # if self.random_image:
+        #     label1['text'] = self.random_image
+        #     label1.update()
+
+        # label1 = tk.Label(root, text='ラベル１')
+        # label1.place(x=100, y=50)
+
+        # img = Image.open('\\Users\\hydra\\Documents\\★好きなもの（Twitterアーカイブ）\\写真\\ふりーな_kan_me.png')
+   
+
+        # self.display_image(app)
+        # if self.app.random_image:
+        #     label1.update()
+        #     label1['text'] = self.app.random_image
+        # global img
+        # img = Image.open('fuyo.png')
+        # img = ImageTk.PhotoImage(img)
+        # canvas.create_image(0, 0, image=img, anchor=tk.NW)
+
+        # print(self.app.random_image)
+        # self.display_image(root,self.app)
 
         # 【作業】ランダムで1つ画像を選んで変数に格納→Image.openの引数に
 # 【作業】RandomViewerクラスのオブジェクトを作成して変数を使用する
-    def display_image(self):
-        pass
-        # print(app.random_image)
-        # img = Image.open('hoge.jpg')
+    # def display_image(self,root):
+        # print(self.app.random_image)
+        # current_img = relative_to(self.app.random_image)
+        # img = Image.open('C:\\Users\\hydra\\Documents\\★好きなもの（Twitterアーカイブ）\\写真\\fuyo.png')
 
         # # 画像のサイズを取得
         # w = img.width
         # h = img.height
 
         # # 画像の縦幅がcanvasの縦幅より大きい場合リサイズ
-        # if w > canvas_w:
-        #     img = img.resize((int(w * (canvas_w / w)), int(h * (canvas_w / w))))
+        # if w > self.canvas_w:
+        #     img = img.resize((int(w * (self.canvas_w / w)), int(h * (self.canvas_w / w))))
             
         # # 画像の横幅がcanvasの横幅より大きい場合リサイズ
-        # if h > canvas_h:
-        #     img = img.resize((int(w * (canvas_h / h)), int(h * (canvas_h / h))))
+        # if h > self.canvas_h:
+        #     img = img.resize((int(w * (self.canvas_h / h)), int(h * (self.canvas_h / h))))
 
         # # 画像を表示
-        # img = ImageTk.PhotoImage(img)
-        # canvas.create_image(0, 0, image=img, anchor=tk.NW)
+
+        # display_img = ImageTk.PhotoImage(img)
+        # self.canvas.create_image(0, 0, image=display_img, anchor=tk.NW)
 
 
 # tkオブジェクトの作成
@@ -113,7 +159,7 @@ root = tk.Tk()
 root.geometry('1000x600+200+50')
 
 # クラスの呼び出し？
-Window(root)
+RandomViewerApp(root)
 
 # メインループの実行
 root.mainloop()
