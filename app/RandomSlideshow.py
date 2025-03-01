1d265f92e9f84087dd16a7c9a8e57656897bc112
knk-summer <163017021+knk-summer@users.noreply.github.com>
2025-03-01 17:14:34 +0900

    canvas左クリックで別ウィンドウで原寸画像表示する処理追加

--- app/RandomSlideshow.py
+++ app/RandomSlideshow.py
@@ -42,42 +42,50 @@ class RandomViewerApp:
         # フォルダ未選択時にエラーメッセージボックス表示
         else:
             messagebox.showerror('エラー','フォルダを選択してください')
-            
+
+    # 格納した絶対パスからランダムで１つ取り出す      
     def random_select_images(self, paths):
         self.random_image = random.choice(paths)
-        self.random_image = self.random_image.replace('\\','/')
-        self.random_image = self.random_image.replace('/','//')
         self.display_image()
 
+    # 選出した画像を表示
     def display_image(self):
-        img = Image.open(self.random_image)
+        # Image.openで開ける形になるように文字列を置換
+        self.random_image = self.random_image.replace('\\','/')
+        self.random_image = self.random_image.replace('/','//')
+        self.op_img = Image.open(self.random_image)
 
         # 画像のサイズを取得
-        w = img.width
-        h = img.height
-        print(str(img.width) + '×' + str(img.height))
+        w = self.op_img.width
+        h = self.op_img.height
+        print(str(self.op_img.width) + '×' + str(self.op_img.height))
 
         # 画像の横幅がcanvasの横幅より大きい場合リサイズ
         if w > self.canvas_w:
-            img = img.resize((int(w * (self.canvas_w / w)), int(h * (self.canvas_w / w))))
-            w = img.width
-            h = img.height
+            self.resize_img = self.op_img.resize((int(w * (self.canvas_w / w)), int(h * (self.canvas_w / w))))
+            w = self.resize_img.width
+            h = self.resize_img.height
             print('画像の横幅がcanvasの横幅より大きい場合リサイズ')
-            print(str(img.width) + '×' + str(img.height))
+            print(str(self.resize_img.width) + '×' + str(self.resize_img.width))
             
         # 画像の縦幅がcanvasの縦幅より大きい場合リサイズ
         if h > self.canvas_h:
-            img = img.resize((int(w * (self.canvas_h / h)), int(h * (self.canvas_h / h))))
-            w = img.width
-            h = img.height
+            self.resize_img = self.op_img.resize((int(w * (self.canvas_h / h)), int(h * (self.canvas_h / h))))
+            w = self.resize_img.width
+            h = self.resize_img.height
             print('画像の縦幅がcanvasの縦幅より大きい場合リサイズ')
         
-        print(str(img.width) + '×' + str(img.height))
+        print(str(self.resize_img.width) + '×' + str(self.resize_img.height))
 
         # 画像を表示
 
-        self.display_img = ImageTk.PhotoImage(img)
+        self.display_img = ImageTk.PhotoImage(self.resize_img)
         self.canvas.create_image((self.canvas_w - w) / 2, (self.canvas_h - h) / 2, image=self.display_img, anchor=tk.NW)
+        self.canvas.bind('<Button-1>',self.image_clicked)
+    
+    # 画像クリック時の処理（引数にevent必須！）※
+    def image_clicked(self,event):
+        self.op_img.show()
 
     def wedget(self,root):
         # ウィンドウのタイトル
@@ -103,7 +111,7 @@ class RandomViewerApp:
         # canvas（画像表示範囲）の作成
         self.canvas_w = 600
         self.canvas_h = 600
-        self.canvas = tk.Canvas(root, width=self.canvas_w, height=self.canvas_h,bg='#E7E7E7')
+        self.canvas = tk.Canvas(root, width=self.canvas_w, height=self.canvas_h, bg='#E7E7E7', cursor='hand2')
         self.canvas.place(x=0, y=0)
 
 # tkオブジェクトの作成
